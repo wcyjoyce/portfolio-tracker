48437838ad35f1bd6eee7141fe1ae2c69cefd1cb
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-08 19:02:40 +0800

    CRUD and routes for portfolios done

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,56 @@
 class PortfoliosController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+    authorize @portfolio
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    @portfolio.user = current_user
+    authorize @portfolio
+    if @portfolio.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    authorize @portfolio
+  end
+
+  def edit
+    authorize @portfolio
+  end
+
+  def update
+    authorize @portfolio
+  end
+
+  def destroy
+    authorize @portfolio
+    @portfolio.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name)
+  end
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :portfolios
+
   get "search", to: "stocks#search"
   get "result", to: "stocks#result"
 
