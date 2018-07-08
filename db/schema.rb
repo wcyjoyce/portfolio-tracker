71b180efa1eafed5bb0fcccc97d5ec1bca6c4ded
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-08 17:35:00 +0800

    model and controller for portfolios done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702035440) do
+ActiveRecord::Schema.define(version: 20180708093025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"
