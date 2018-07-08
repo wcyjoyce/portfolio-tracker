40ab11b4f9922867aece5fd342e99a141b35d944
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-09 02:08:50 +0800

    portfolio schema done

--- db/migrate/20180708173917_create_portfolios.rb
+++ db/migrate/20180708173917_create_portfolios.rb
@@ -2,6 +2,7 @@ class CreatePortfolios < ActiveRecord::Migration[5.1]
   def change
     create_table :portfolios do |t|
       t.string :name
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708093025) do
+ActiveRecord::Schema.define(version: 20180708173917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "portfolios", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
   end
 
   create_table "stocks", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 20180708093025) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "portfolios", "users"
 end
