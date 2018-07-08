f15fe9e01601a8c164d547440c9afb451ece4bc6
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-08 17:20:25 +0800

    setup with IE Trading API done

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,9 @@
+require "json"
+require "open-uri"
+
 class StocksController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:search, :result]
+
   # def search
   #   if params[:stock]
   #     @stock = Stock.find_by_ticker(params[:stock])
@@ -15,7 +19,14 @@ class StocksController < ApplicationController
 
   def result
     @query = params[:stock]
+
+    # stock_quote gem
     @result = StockQuote::Stock.quote(@query)
+
+    # IE Trading API
+    url = "https://api.iextrading.com/1.0/stock/#{@query}/company"
+    result = JSON.parse(open(url).read)
+    @tags = "#{result['tags']}"
   end
 
   def search
