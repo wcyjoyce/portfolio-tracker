393c0b4cb1f239de9cc28318c1204186e2b5ef43
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-08 16:27:36 +0800

    basic stock search done

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,23 @@
 class StocksController < ApplicationController
+  skip_before_action :authenticate_user!
+  # def search
+  #   if params[:stock]
+  #     @stock = Stock.find_by_ticker(params[:stock])
+  #     @stock ||= Stock.new_from_lookup(params[:stock])
+  #   end
+
+  #   if @stock
+  #     render "stocks/lookup"
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
+
+  def result
+    @query = params[:stock]
+    @result = StockQuote::Stock.quote(@query)
+  end
+
+  def search
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+  # def self.find_by_ticker(ticker_symbol)
+  #   where(ticker: ticker_symbol).first
+  # end
+
+  # def self.new_from_lookup(ticker_symbol)
+  #   looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+  #   return nil unless looked_up_stock.name
+  #   new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+  #   new_stock
+  # end
 end
