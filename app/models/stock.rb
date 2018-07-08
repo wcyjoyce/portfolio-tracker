809eefb743cfac20cf9a160e4458c57853c04f7d
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-09 04:59:37 +0800

    more details added for stock holdings in portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,24 @@ class Stock < ApplicationRecord
   def name(ticker)
     StockQuote::Stock.quote(ticker).company_name
   end
+
+  def current_price(ticker)
+    StockQuote::Stock.quote(ticker).latest_price
+  end
+
+  def total_cost
+    total_cost = shares * price
+  end
+
+  def market_value
+    market_value = shares * current_price(ticker)
+  end
+
+  def profit_amount
+    profit_amount = market_value - total_cost
+  end
+
+  def profit_pct
+    profit_amount / total_cost * 100
+  end
 end
