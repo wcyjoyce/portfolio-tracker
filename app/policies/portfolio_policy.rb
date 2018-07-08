734f902bd5152acc07b654d67ac9a81f7b50ffe6
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-09 02:16:50 +0800

    pundit policies for portfolio and stock models done

---
+++ app/policies/portfolio_policy.rb
@@ -0,0 +1,7 @@
+class PortfolioPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
---
+++ app/policies/stock_policy.rb
@@ -0,0 +1,7 @@
+class StockPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
