6d73079e33bb442dd2bcdc2bcda4309b02cfb08d
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-15 13:20:37 +0800

    added conditional to stop code breaking if no query was passed in search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,55 +9,57 @@ class StocksController < ApplicationController
   def result
     @query = params[:stock]
 
-    ## stock_quote gem
-    # @result = StockQuote::Stock.quote(@query)
+    unless @query.empty? || @query.nil?
+      ## stock_quote gem
+      # @result = StockQuote::Stock.quote(@query)
 
-    # IE Trading API
-    company_url = "https://api.iextrading.com/1.0/stock/#{@query}/company"
-      company = JSON.parse(open(company_url).read)
-      @name = "#{company['companyName']}"
-      @symbol = "#{company['symbol']}"
-      @exchange = "#{company['exchange']}"
-      @sector = "#{company['sector']}"
-      @description = "#{company['description']}"
+      # IE Trading API
+      company_url = "https://api.iextrading.com/1.0/stock/#{@query}/company"
+        company = JSON.parse(open(company_url).read)
+        @name = "#{company['companyName']}"
+        @symbol = "#{company['symbol']}"
+        @exchange = "#{company['exchange']}"
+        @sector = "#{company['sector']}"
+        @description = "#{company['description']}"
 
-    book_url = "https://api.iextrading.com/1.0/stock/#{@query}/book"
-      book = JSON.parse(open(book_url).read)
-      @update = Time.at("#{book['quote']['latestUpdate']}".to_i).to_datetime.strftime("%e %b %Y %H:%M:%S%p")
-      @price = "#{book['quote']['latestPrice']}"
-      @open = "#{book['quote']['open']}"
-      @close = "#{book['quote']['close']}"
-      @high = "#{book['quote']['high']}"
-      @low = "#{book['quote']['low']}"
-      @market_cap = "#{book['quote']['marketCap']}"
-      @pe_ratio = "#{book['quote']['peRatio']}"
+      book_url = "https://api.iextrading.com/1.0/stock/#{@query}/book"
+        book = JSON.parse(open(book_url).read)
+        @update = Time.at("#{book['quote']['latestUpdate']}".to_i).to_datetime.strftime("%e %b %Y %H:%M:%S%p")
+        @price = "#{book['quote']['latestPrice']}"
+        @open = "#{book['quote']['open']}"
+        @close = "#{book['quote']['close']}"
+        @high = "#{book['quote']['high']}"
+        @low = "#{book['quote']['low']}"
+        @market_cap = "#{book['quote']['marketCap']}"
+        @pe_ratio = "#{book['quote']['peRatio']}"
 
-    historical_url = "https://api.iextrading.com/1.0/stock/#{@query}/chart/5y"
-      historical = JSON.parse(open(historical_url).read)
-      @chart = historical.collect { |historical| [historical['date'], historical['close']] }
-      @library_options = {
-        xAxis: {
-          crosshair: true,
-          title: {text: "date"}
-        },
-        yAxis: {
-          crosshair: true,
-          title: {text: "price"},
+      historical_url = "https://api.iextrading.com/1.0/stock/#{@query}/chart/5y"
+        historical = JSON.parse(open(historical_url).read)
+        @chart = historical.collect { |historical| [historical['date'], historical['close']] }
+        @library_options = {
+          xAxis: {
+            crosshair: true,
+            title: {text: "date"}
+          },
+          yAxis: {
+            crosshair: true,
+            title: {text: "price"},
+          }
         }
-      }
 
-    stats_url = "https://api.iextrading.com/1.0/stock/#{@query}/stats"
-      stats = JSON.parse(open(stats_url).read)
-      @high_52 = "#{stats['week52high']}"
-      @low_52 = "#{stats['week52low']}"
-      @beta = "#{stats['beta']}"
-      @dividend_yield = "#{stats['dividendYield']}"
-      @eps = "#{stats['latestEPS']}"
-      @roe = "#{stats['returnOnEquity']}"
-      @eps_date = "#{stats['latestEPSDate']}"
+      stats_url = "https://api.iextrading.com/1.0/stock/#{@query}/stats"
+        stats = JSON.parse(open(stats_url).read)
+        @high_52 = "#{stats['week52high']}"
+        @low_52 = "#{stats['week52low']}"
+        @beta = "#{stats['beta']}"
+        @dividend_yield = "#{stats['dividendYield']}"
+        @eps = "#{stats['latestEPS']}"
+        @roe = "#{stats['returnOnEquity']}"
+        @eps_date = "#{stats['latestEPSDate']}"
 
-    comps_url = "https://api.iextrading.com/1.0/stock/#{@query}/peers"
-      @comps = JSON.parse(open(comps_url).read)
+      comps_url = "https://api.iextrading.com/1.0/stock/#{@query}/peers"
+        @comps = JSON.parse(open(comps_url).read)
+    end
   end
 
   def search
