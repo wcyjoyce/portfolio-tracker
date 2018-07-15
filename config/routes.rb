53ed4a6c7cc9b1e4582e4c51670be55e6ced8c10
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-07-15 18:06:17 +0800

    about route created

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   get "users/:id/dashboard", to: "users#dashboard", as: "dashboard"
   get "users/:id/transactions", to: "users#transactions", as: "transactions"
 
+  get "pages/home", to: "pages#home", as: "home"
+  get "pages/about", to: "pages#about", as: "about"
+
   root to: "pages#home"
 end
