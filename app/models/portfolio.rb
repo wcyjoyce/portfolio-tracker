f2bbec6cfb6de40237585d04884e7bb509174091
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-08-04 18:57:24 +0800

    inception date and annualised return added

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :stocks, dependent: :destroy
@@ -23,5 +25,28 @@ class Portfolio < ApplicationRecord
 
   def profit_pct
     profit_amount / total_cost * 100
+  end
+
+  def inception
+    stocks.order(added: :asc).first.added.strftime("%d %b %Y")
+  end
+
+  def annualised
+    incepted_seconds = Date.today.to_time - inception.to_time
+    incepted_days = incepted_seconds / 60 / 60 / 24
+    annualised_return = ((100 + profit_pct) ** (365 / incepted_days)) - 100
+    annualised_return.round(2)
+  end
+
+  def duplicates
+    tickers = stocks.map { |stock| stock.ticker }.uniq
+      # array = stocks.select { |stock| ticker == stock.ticker }
+  #     # .map do |dummy, qty, price|
+  #     #   [qty, (price * qty)]
+  #     # end
+  #     # total_qty = selected_array.sum{ |ele| ele.ticker }
+  #     # total_price = selected_array.sum{ |ele| ele.price }
+  #     # [ticker, total_qty,total_price / total_qty]
+    # end
   end
 end
