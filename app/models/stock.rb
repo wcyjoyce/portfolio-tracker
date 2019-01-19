b80c9ca64b061193c6f85b1dec37f1604b026465
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-01-20 00:05:59 +0800

    day_chart and country methods added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,6 +8,13 @@ class Stock < ApplicationRecord
     chart = historical.collect { |historical| [historical["date"], historical["close"]] }
   end
 
+  def self.day_chart(stock)
+    historical_url = "https://api.iextrading.com/1.0/stock/#{stock}/chart/1d"
+    historical = JSON.parse(open(historical_url).read)
+    chart = historical.collect { |historical| [historical["minute"], historical["close"]] }
+    # chart.reject { |price| price[1].null? }
+  end
+
   def self.comps(stock)
     comps_url = "https://api.iextrading.com/1.0/stock/#{stock}/peers"
     comps = JSON.parse(open(comps_url).read)
@@ -16,5 +23,9 @@ class Stock < ApplicationRecord
   def self.news(stock)
     news_url = "https://api.iextrading.com/1.0/stock/#{stock}/news"
     news = JSON.parse(open(news_url).read)
+  end
+
+  def country
+    ticker.match(/^[a-zA-Z]/) ? "US" : "HK"
   end
 end
