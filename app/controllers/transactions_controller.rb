765e695c05c07afc8e9cb34057a618eca1753e90
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-09 02:16:11 +0800

    TODO removed

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,6 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: [:edit, :update, :destroy]
 
-  # TODO: check routes
-
   def index
     @user = current_user
     @transactions = Transaction.all
