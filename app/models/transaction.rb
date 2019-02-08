6480099bb7803010898b9ada4f52d9db9d4271ed
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-09 02:14:37 +0800

    historical_price method made dynamic, allocation method added

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -19,13 +19,12 @@ class Transaction < ApplicationRecord
     errors.add(:deleted_at, "Your transaction cannot be in the future") if deleted_at.present? && deleted_at > Date.today
   end
 
-  # TODO: historial price is not dynamic
   def historical_price
     month = deleted_at.strftime("%b")
     day = deleted_at.strftime("%d")
     year = deleted_at.strftime("%Y")
-    url = "https://www.investopedia.com/markets/api/partial/historical/?Symbol=FB&Type=%20Historical+Prices&Timeframe=Daily&StartDate=Jan+11%2C+2019&EndDate=Jan+11%2C+2019"
-    # url = "https://www.investopedia.com/markets/api/partial/historical/?Symbol=#{stock}&Type=%20Historical+Prices&Timeframe=Daily&StartDate=#{month}+#{day}%2C+#{year}&EndDate=#{month}+#{day}%2C+#{year}"
+    # url = "https://www.investopedia.com/markets/api/partial/historical/?Symbol=FB&Type=%20Historical+Prices&Timeframe=Daily&StartDate=Jan+11%2C+2019&EndDate=Jan+11%2C+2019"
+    url = "https://www.investopedia.com/markets/api/partial/historical/?Symbol=#{stock.ticker}&Type=%20Historical+Prices&Timeframe=Daily&StartDate=#{month}+#{day}%2C+#{year}&EndDate=#{month}+#{day}%2C+#{year}"
     html = Nokogiri::HTML(open(url).read)
     historical_price = html.css(".in-the-money .num[2]").text.strip
     return historical_price.to_f
@@ -70,6 +69,10 @@ class Transaction < ApplicationRecord
 
   def profit_pct
     profit_amount / total_cost * 100
+  end
+
+  def allocation
+    (market_value / portfolio.market_value * 100).round(2)
   end
 
   def ytd(ticker)
