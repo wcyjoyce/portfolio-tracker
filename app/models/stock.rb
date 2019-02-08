0874a976d2dc656222118377ec694903bf2e33f3
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-08 17:34:13 +0800

    responsive view for comps done

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -18,6 +18,46 @@ class Stock < ApplicationRecord
   def self.comps(stock)
     comps_url = "https://api.iextrading.com/1.0/stock/#{stock}/peers"
     comps = JSON.parse(open(comps_url).read)
+    comps.first(5).include?(comps.last) ? comps = comps.first(5).unshift(stock) : comps = comps.first(5).unshift(stock) << comps.last
+    comps_table = []
+
+    comps.each do |comp|
+      stats = JSON.parse(open("https://api.iextrading.com/1.0/stock/#{comp}/stats").read)
+      book = JSON.parse(open("https://api.iextrading.com/1.0/stock/#{comp}/book").read)
+
+      market_cap = ("#{book['quote']['marketCap']}").to_f
+      market_cap >= 1000000000 ? market_cap = (market_cap / 1000000000).round(2).to_s + "B" : market_cap = (market_cap / 1000000).round(2).to_s + "M"
+
+      row = [
+        name = "#{stats['companyName']}",
+        ticker = comp,
+        latest_price = ("#{book['quote']['latestPrice']}").to_f,
+        market_cap,
+        day_change = ("#{book['quote']['changePercent']}".to_f * 100).round(2),
+        price_range = "#{book['quote']['high']}" + " / " + "#{book['quote']['low']}",
+        ytd_change = ("#{book['quote']['ytdChange']}".to_f * 100).round(2),
+        pe_ratio = ("#{book['quote']['peRatio']}").to_f,
+        beta = ("#{stats['beta']}".to_f).round(2),
+        roe = ("#{stats['returnOnEquity']}").to_f,
+        eps = ("#{stats['latestEPS']}").to_f,
+        # dividend_yield = ("#{stats['dividendYield']}".to_f).round(2)
+      ]
+      row.map! { |r| r == 0 ? "-" : r}
+      comps_table << row
+    end
+    comps_table
+  end
+
+  def self.comps_mobile(stock)
+    # ticker, price range, market cap, YTD
+    comps_table = Stock.comps(stock)
+    mobile_comps = []
+    comps_table.each do |detail|
+      row = []
+      row << detail[1] << detail[5] << detail[3] << detail[6]
+      mobile_comps << row
+    end
+    mobile_comps
   end
 
   def self.news(stock)
