f67b343381c87e0ba6d65ebd9889cde891f1cf87
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-09 02:06:36 +0800

    formatted method output classes to avoid code breaking

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,24 +17,24 @@ class User < ApplicationRecord
 
   def market_value
     market_value = 0
-    portfolios.each { |portfolio| market_value += portfolio.market_value }
-    market_value
+    portfolios.each { |portfolio| market_value += portfolio.market_value.to_f }
+    market_value.to_f.zero? ? "-" : market_value.to_i
   end
 
   def profit_amount
     profit_amount = 0
-    portfolios.each { |portfolio| profit_amount += portfolio.profit_amount }
-    profit_amount
+    portfolios.each { |portfolio| profit_amount += portfolio.profit_amount.to_f }
+    profit_amount.to_f.zero? ? "-" : profit_amount
   end
 
   def profit_pct
     total_cost = 0
-    portfolios.each { |portfolio| total_cost += portfolio.total_cost }
-    (profit_amount.to_f / total_cost.to_f * 100).round(2)
+    portfolios.each { |portfolio| total_cost += portfolio.total_cost.to_f }
+    total_cost.to_f.zero? ? "-" : (profit_amount.to_f / total_cost.to_f * 100).round(2)
   end
 
   def display(number)
-    number > 0 ? "positive" : "negative"
+    number.to_f > 0 ? "positive" : "negative"
   end
 
   def sign(number)
